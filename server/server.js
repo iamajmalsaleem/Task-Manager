a88b780ba4a977537f9a9534b9ab374d52c45551
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-02-23 21:08:52 +0530

    Setup React and Node

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 7000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(port, () =>{
-    console.log(`Server listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
